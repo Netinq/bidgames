5d616e3fe1d89706be0c2dad99917657c33a1bad
BuildTools <sarquentin@gmail.com>
2021-12-10 00:40:58 +0100

    Fix

--- api/middlewares/AppToken.js
+++ api/middlewares/AppToken.js
@@ -3,6 +3,6 @@ const ErrorMessage = require("../errors/ErrorMessage");
 module.exports = async (req, res, next) => {
   const token = req.header("app-token");
   if (token != process.env.TOKEN)
-    throw new ErrorMessage("Invalid token").send(res);
+    return new ErrorMessage("Invalid token").send(res);
   next();
 };
